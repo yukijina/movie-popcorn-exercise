e57af48e1f33954a922d5a2885d200e3f8c83c41
yuki <38411009+yukijina@users.noreply.github.com>
2026-01-21 06:42:22 -0800

    fixed rating paragraph, added comment for svg star

--- src/StarRating.js
+++ src/StarRating.js
@@ -35,7 +35,7 @@ export default function StarRating({ maxRating = 5 }) {
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || ''}</p>
+      <p style={textStyle}>{tempRating || rating || ''}</p>
     </div>
   );
 }
@@ -57,6 +57,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
       onMouseLeave={onHoverOut}
     >
       {full ? (
+        // full star
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
@@ -66,6 +67,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
         </svg>
       ) : (
+        // empty star
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
