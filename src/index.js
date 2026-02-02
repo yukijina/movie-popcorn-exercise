d54ede2009c7bf3a0d876bff8101c093e3def531
yuki <38411009+yukijina@users.noreply.github.com>
2026-02-02 06:38:25 -0800

    hide the starRating component - will integrate later

--- src/index.js
+++ src/index.js
@@ -6,27 +6,27 @@ import StarRating from './StarRating';
 import { useState } from 'react';
 
 // use movie rating in other component so pass the rating as props
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color='blue' maxRating={10} setMovieRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color='blue' maxRating={10} setMovieRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
       defaultRating={3}
     />
     <StarRating maxRating={10} className='test' />
     <StarRating />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
